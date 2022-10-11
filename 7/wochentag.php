6bbede3ab3810d4cbec41bd0f2a7abba856af8ac
Rosario Polito <rosario.polito@gmx.de>
2022-10-11 19:20:40 +0200

    adding weekday

---
+++ 7/wochentag.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $date = date("N");
+
+        if($date > 5){
+            echo "Schönes Wochenende";
+        } {
+            echo "Schönen Arbeitstag to looser!";
+        }
+    ?>
+    
+</body>
+</html>
